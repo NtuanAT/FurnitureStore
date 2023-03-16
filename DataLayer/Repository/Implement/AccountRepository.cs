c7bafe51a53bff1de285880b5727ca2f7c01a7d3
NtuanAT <tuannase160161@fpt.edu.vn>
2023-03-16 14:59:31 +0700

    repo base

---
+++ DataLayer/Repository/Implement/AccountRepository.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Entities;
+using DataLayer.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository.Implement
+{
+	public class AccountRepository : RepositoryBase<Account>
+	{
+	
+	}
+}
---
+++ DataLayer/Repository/Interface/RepositoryBase.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Repository.Interface
+{
+	public class RepositoryBase<T> where T : class
+	{
+		private	StoreDBContext _storeDBContext;
+		public DbSet<T> Set { get; set; }
+		public RepositoryBase()
+		{
+			_storeDBContext = new();
+			Set = _storeDBContext.Set<T>();
+		}
+		public T Get(Expression<Func<T,bool>> predicate)
+		{
+			return Set.Where(predicate).FirstOrDefault();
+		}
+		public void Delete(Expression<Func<T,bool>> predicate)
+		{
+			
+		}
+		public void Update(T entity)
+		{
+			Set.Attach(entity);
+			_storeDBContext.Entry(entity).State = EntityState.Modified;
+			_storeDBContext.SaveChanges();
+		}
+		public List<T> GetAll()
+		{
+			return Set.ToList();
+		}
+		public void Create(T entity)
+		{
+			Set.Add(entity);
+			_storeDBContext.SaveChanges();
+		}
+
+	}
+}
